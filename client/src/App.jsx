5e8ef578da22014f6ddd1873abfa9b8a33efc681
Shubham Singh <shubhamrajput2702@gmail.com>
2026-03-25 15:08:04 +0530

    Split route pages with lazy loading for faster first load

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,28 @@
+import { lazy, Suspense } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Recommend from './pages/Recommend'
-import Login from './pages/Login'
+
+const Home = lazy(() => import('./pages/Home'))
+const Recommend = lazy(() => import('./pages/Recommend'))
+const Login = lazy(() => import('./pages/Login'))
 
 function App() {
   return (
     <div className="min-h-screen bg-dark-900">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/recommend" element={<Recommend />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="flex min-h-[60vh] items-center justify-center text-gray-600 dark:text-gray-400">
+            Loading...
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/recommend" element={<Recommend />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Suspense>
     </div>
   )
 }
